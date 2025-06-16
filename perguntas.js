f7c95993d619dc73584a7d43c6599e011aa77764
ellen2102 <anny.filadelfo@escola.pr.gov.br>
2025-06-16 16:59:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'geografia',
+    'qua é o maior país do mundo em extensão territorial?',
+    'Rússia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'geografia',
+    'qual o maior rio do mundo em volume de água?',
+     'Rio Amazonas'
+)
+
+criaCartao(
+    'geografia',
+    'para que serve um mapa?',
+    'Para mostrar lugares e ajudar na localização'
+)
+criaCartao(
+    'Geografia',
+    'Qual é o lugar mair frio do planeta?',
+    'Antártida'
+)
+
+criaCartao(
+    'geografia',
+    'quantos estados tem o Brasil?',
+    '26 Estados e 1 Distrito Federal'
+)
